a9092826d186c667fbac337b32825bdf58e2203c
e200106 <cyrilauquier@hotmail.fr>
2022-12-08 08:14:07 +0100

    Création de BookSave.cs

--- GBReaderAuquierC.Domains/BookSave.cs
+++ GBReaderAuquierC.Domains/BookSave.cs
@@ -2,6 +2,49 @@
 {
     public class BookSave
     {
+        private DateTime _begin;
+        private DateTime _lastUpdate;
+
+        private IList<int> _history = new List<int>();
+
+        public IList<int> History
+        {
+            get => new List<int>(_history);
+            private set => _history = value;
+        }
+
+        public DateTime LastUpdate
+        {
+            get => _lastUpdate;
+            private set => _lastUpdate = value;
+        }
+
+        public DateTime Begin
+        {
+            get => _begin;
+            private set => _begin = value;
+        }
         
+        public static BookSave operator +(BookSave s, int b)
+        {
+            s.History.Add(b);
+            s.LastUpdate = DateTime.Now;
+            return s;
+        }
+
+        public BookSave()
+        {
+            _begin = DateTime.Now;
+            _lastUpdate = DateTime.Now;
+        }
+
+        public static BookSave Get(DateTime begin, DateTime lastUpdate, IList<int> history)
+        {
+            BookSave result = new();
+            result.History = history;
+            result.LastUpdate = lastUpdate;
+            result.Begin = begin;
+            return result;
+        }
     }
 }
